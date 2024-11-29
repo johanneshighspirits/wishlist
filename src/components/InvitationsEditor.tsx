71e8eaf504a693e4e08449879f6f8dfde8b24214
Johannes Borgström <johannes.borgstrom@1337.tech>
2024-11-29 05:22:39 +0100

    Adds logging

--- src/components/InvitationsEditor.tsx
+++ src/components/InvitationsEditor.tsx
@@ -1,8 +1,8 @@
-import { getServerUserEmail } from '@/lib/auth';
-import { getKeyUserInvitations } from '@/lib/wishlists/constants';
-import { Invitation } from '@/lib/wishlists/types';
-import { kv } from '@vercel/kv';
-import { InvitationItem } from './InvitationItem';
+import { getServerUserEmail } from "@/lib/auth";
+import { getKeyUserInvitations } from "@/lib/wishlists/constants";
+import { Invitation } from "@/lib/wishlists/types";
+import { kv } from "@vercel/kv";
+import { InvitationItem } from "./InvitationItem";
 
 export const InvitationsEditor = async ({
   showMessageIfEmpty,
@@ -11,18 +11,23 @@ export const InvitationsEditor = async ({
 }) => {
   const userEmail = await getServerUserEmail();
   const invitationKeys = await kv.smembers<string[]>(
-    getKeyUserInvitations(userEmail)
+    getKeyUserInvitations(userEmail),
   );
+  console.log(`email: ${userEmail} invited to ${invitationKeys.join(", ")}`);
   const invitationRequests = await Promise.all<Promise<Invitation | null>[]>(
-    invitationKeys.map((key) => kv.get<Invitation | null>(key))
+    invitationKeys.map((key) => kv.get<Invitation | null>(key)),
   );
   const invitations = invitationRequests?.filter(
-    (inv) => inv !== null && !(inv.isDeclined || inv.isAccepted)
+    (inv) => inv !== null && !(inv.isDeclined || inv.isAccepted),
   ) as Invitation[];
+  console.log(`email: ${userEmail} found ${invitations.length} invitations`);
 
   if (invitations.length === 0) {
     return showMessageIfEmpty ? (
-      <div>Det finns inga inbjudningar här...</div>
+      <div>
+        <h2>Det finns inga inbjudningar här...</h2>
+        <p>En inbjudan är bara giltig i en vecka</p>
+      </div>
     ) : null;
   }
   return (
